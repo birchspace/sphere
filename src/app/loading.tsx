f347bac43fd5265c4c6eacfec6628975247be0c5
Birch <birch17261@gmail.com>
2023-10-22 17:20:50 +0800

    feat: update style

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex h-[50vh] max-w-[854px] items-center justify-center py-8 lg:w-4/5">
+    <div className="flex h-screen w-screen items-center justify-center py-8 lg:w-4/5">
       <div className="h-12 w-12 animate-spin rounded-full border border-solid border-yellow-500 border-t-transparent"></div>
     </div>
   );
